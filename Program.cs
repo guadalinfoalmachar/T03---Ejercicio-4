8cdd0c5772e2d74527a3e259a17341cb644eab9b
Juan Manuel España <guadalinfo.almachar@guadalinfo.es>
2022-06-28 18:41:37 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace T03_Eje4
+{
+    class T03_Eje4
+    {
+        static void Main()
+        {
+            int N = 4;
+
+            Console.WriteLine("Valor inicial de N = {0}", N);
+            Console.WriteLine("N + 77 = {0}", N += 77);
+            Console.WriteLine("N - 3 = {0}", N -= 3);
+            Console.WriteLine("N * 2 = {0}", N * 2);
+
+
+            Console.WriteLine("Pulse cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
